b41d7ea2fc26d7dda0ca770e097c627afd6c3eb8
maryjean <maryjanecugal@gmail.com>
2016-09-21 02:33:09 +0800

    Update Date.java

--- Date.java
+++ Date.java
@@ -27,18 +27,27 @@ public class Date{
 		if (year < 1000 && year > 9999){
 			throw new IllegalArgumentException("error in setting year");
 		}
-	System.out.println(year);
+		System.out.println(year);
 	}
 	public void setMonth(int month){
 		if (month <= 1 && month >= 12){
 			throw new IllegalArgumentException("error in setting month");
 		}
-
 		System.out.println(month);
 	}
 	public void setDay(int day){
-			
-	
+		if ((month == 4 || month == 6 || month == 8 || month == 10 || month == 12) && (day < 1 && day > 30)){
+			throw new IllegalArgumentException("this month has only 31 days!");
+		
+		}else if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 9 || month == 11) && (day < 1 && day > 31)){
+			throw new IllegalArgumentException("this month has only 31 days!");
+		}
+		else if ((leap(year) == true)  && (month == 2) && (day<1 || day > 28)){
+			throw new IllegalArgumentException("this is leap year. february has only 28 days!");
+		}else if ((leap(year) == false) && (month == 2) && (day < 1 || day > 29){
+			throw new IllegalArgumentException("this is leap year. february has only 29 days!");
+		}
+		System.out.println(day);
 	}
 	public bool leap(int year){
 		if ((year % 4 == 0) || (year % 400 != 0 && year % 100 == 0)){
